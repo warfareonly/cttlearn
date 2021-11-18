a578bd42fd1cf310a8184e1dfd28c4feebd578b8
Diego Damasceno <d.damasceno@cs.ru.nl>
2021-11-18 22:03:02 +0100

    exectime using `date +%s.%N`

--- experiments/01check_exec_time.sh
+++ experiments/01check_exec_time.sh
@@ -2,18 +2,23 @@
 
 PARSER="python3 ../src/dot2soucha_parser/dot2soucha.py"
 MODEL_DIR="../data/2021_11_ctt_automatawiki"
-FSMLIB="../src/fsm_lib -debug"
+FSMLIB="../src/fsm_lib"
+A_LOG="stats.log"
+
+rm -f $A_LOG
+echo "FSM\tCTT\tEXTRA_STATES\tEXEC_TIME" > $A_LOG 
 
 for EXTRA_STATES in 0 1 2 3; do
     for CTT in soucha_w soucha_wp soucha_h soucha_hsi soucha_spy soucha_spyh; do
 # for EXTRA_STATES in 0; do
 #     for CTT in soucha_w soucha_hsi soucha_spy; do
-        A_LOG="stats_${CTT}_es${EXTRA_STATES}.log"
-        rm -f $A_LOG
-        for SUL in  Bankcard DTLS MQTT QUICprotocol SSH TCP TLS; do 
+    for SUL in  Bankcard DTLS MQTT QUICprotocol SSH TCP TLS; do 
             for F_DOT in "${MODEL_DIR}/$SUL"/*.dot; do 
-                echo "fsm_model:\t${F_DOT}" >> $A_LOG
-                $PARSER -f "${F_DOT}" | $FSMLIB -m $CTT -es $EXTRA_STATES 2>> $A_LOG > /dev/null
+                start=`date +%s.%N`
+                $PARSER -f "${F_DOT}" | $FSMLIB -m $CTT -es $EXTRA_STATES 2>&1 /dev/null
+                end=`date +%s.%N`
+                runtime=$( echo "$end - $start" | bc -l )
+                echo "${F_DOT}\t${CTT}\t${EXTRA_STATES}\t${runtime}" >> $A_LOG 
             done
         done
     done
