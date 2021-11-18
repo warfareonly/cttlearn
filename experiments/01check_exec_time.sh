ed71cf8ca9d1868421145aa7c240553427ebcec7
Diego Damasceno <d.damasceno@cs.ru.nl>
2021-11-18 20:46:49 +0100

    debug mode

--- experiments/01check_exec_time.sh
+++ experiments/01check_exec_time.sh
@@ -2,7 +2,7 @@
 
 PARSER="python3 ../src/dot2soucha_parser/dot2soucha.py"
 MODEL_DIR="../data/2021_11_ctt_automatawiki"
-FSMLIB="../src/fsm_lib"
+FSMLIB="../src/fsm_lib -debug"
 
 for EXTRA_STATES in 0 1 2 3; do
     for CTT in soucha_w soucha_wp soucha_h soucha_hsi soucha_spy soucha_spyh; do
@@ -13,7 +13,7 @@ for EXTRA_STATES in 0 1 2 3; do
         for SUL in  Bankcard DTLS MQTT QUICprotocol SSH TCP TLS; do 
             for F_DOT in "${MODEL_DIR}/$SUL"/*.dot; do 
                 echo "fsm_model:\t${F_DOT}" >> $A_LOG
-                $PARSER -f "${F_DOT}" | $FSMLIB -m $CTT -es $EXTRA_STATES -debug 2>> $A_LOG > /dev/null
+                $PARSER -f "${F_DOT}" | $FSMLIB -m $CTT -es $EXTRA_STATES 2>> $A_LOG > /dev/null
             done
         done
     done
