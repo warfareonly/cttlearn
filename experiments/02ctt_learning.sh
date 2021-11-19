6fbaf454aa9116de6e57fbe8763d8407b4200c01
Diego Damasceno <d.damasceno@cs.ru.nl>
2021-11-19 18:48:52 +0100

    settings for learning are explicit

--- experiments/02ctt_learning.sh
+++ experiments/02ctt_learning.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CTT_LEARNING="java -jar ../src/benchmark.jar -cache"
+CTT_LEARNING="java -jar ../src/benchmark.jar -cache -cexh RivestSchapire -clos CloseFirst -learn lstar"
 MODEL_DIR="../data/2021_11_ctt_automatawiki"
 MAX_PROCS=7
 
@@ -8,7 +8,7 @@ for EXTRA_STATES in 0 1 2 3; do
     for CTT in soucha_w soucha_wp soucha_h soucha_hsi soucha_spy soucha_spyh; do
 # for EXTRA_STATES in 0; do
 #     for CTT in soucha_w soucha_hsi soucha_spy; do
-        for SUL in  Bankcard DTLS MQTT QUICprotocol SSH TCP TLS; do 
+        for SUL in  Bankcard DTLS MQTT QUICprotocol SSH TCP TLS; do
             for F_DOT in "${MODEL_DIR}/${SUL}/"*.dot; do 
                 echo $CTT_LEARNING -eq $CTT -es $EXTRA_STATES -sul "$F_DOT"
             done
