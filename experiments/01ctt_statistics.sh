d52133685f2b383ca62c641d56d1dc075314d6dd
CDN (Diego) Damasceno <d.damasceno@cs.ru.nl>
2021-11-19 11:49:23 +0100

    renaming scripts

--- experiments/01ctt_statistics.sh
+++ experiments/01ctt_statistics.sh
@@ -15,21 +15,21 @@ for EXTRA_STATES in 0 1 2 3; do
     for SUL in  Bankcard DTLS MQTT QUICprotocol SSH TCP TLS; do 
             for F_DOT in "${MODEL_DIR}/$SUL"/*.dot; do 
                 start=`date +%s.%N`
+                # The 'testcase.tmp' file has M lines, one for each test case
+                # The lines follow the format: "tc_x:	0,1,2,3"
                 $PARSER -f "${F_DOT}" | $FSMLIB -m $CTT -es $EXTRA_STATES > testcase.tmp
                 end=`date +%s.%N`
                 runtime=$( echo "$end - $start" | bc -l )
                 
-                # The 'testcase.tmp' file has M lines
-                # There is one for each generate test case
-                # By counting the number of lines, you find the total number of resets/test cases
-                tot_resets=$( wc -l testcase.tmp )
-                
-                # The lines of the 'testcase.tmp' file follow the format: "tc_x:	0,1,2,3"
-                # Then, to calculates the number of symbols, you can use the characters:
+                # Then, to calculates the number of resets and symbols, you can use the characters:
                 #   ':' that separates test case IDs from its respective test sequence
                 #   ',' that separates test symbols
-                # By counting the characters ':,', you find the total number of symbols
-                tot_symbols=$( cat testcase.tmp |tr -cd ':,'| wc -c ) 
+                
+                # By counting the occurences of ':', you find the total number of resets
+                tot_resets=`cat testcase.tmp |tr -cd ':'| wc -c ` 
+                
+                # By counting the ocurrences of both ':,', you find the total number of resets
+                tot_symbols=`cat testcase.tmp |tr -cd ':,'| wc -c ` 
                 echo "${F_DOT}\t${CTT}\t${EXTRA_STATES}\t${tot_resets}\t${tot_symbols}\t${runtime}" >> $A_LOG 
                 rm -f testcase.tmp
             done
