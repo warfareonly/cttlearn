99bbd18e8faab560c8c72b84f16ad1de37f0e42d
Diego Damasceno <d.damasceno@cs.ru.nl>
2021-11-20 09:51:02 +0100

    timeout added to benchmarking process

--- experiments/02ctt_learning.sh
+++ experiments/02ctt_learning.sh
@@ -3,15 +3,16 @@
 CTT_LEARNING="java -jar ../src/benchmark.jar -cache -cexh RivestSchapire -clos CloseFirst -learn lstar"
 MODEL_DIR="../data/2021_11_ctt_automatawiki"
 MAX_PROCS=7
+TIMEOUT=30m
 
-for EXTRA_STATES in 0 1 2 3; do
-    for CTT in soucha_w soucha_wp soucha_h soucha_hsi soucha_spy soucha_spyh; do
-# for EXTRA_STATES in 0; do
-#     for CTT in soucha_w soucha_hsi soucha_spy; do
+# for EXTRA_STATES in 0 1 2; do
+#     for CTT in soucha_w soucha_wp soucha_h soucha_hsi soucha_spy soucha_spyh; do
+for EXTRA_STATES in 0 1; do
+    for CTT in soucha_w soucha_wp soucha_hsi soucha_spy; do
         for SUL in  Bankcard DTLS MQTT QUICprotocol SSH TCP TLS; do
             for F_DOT in "${MODEL_DIR}/${SUL}/"*.dot; do 
                 echo $CTT_LEARNING -eq $CTT -es $EXTRA_STATES -sul "$F_DOT"
             done
         done
     done
-done | xargs -I CMD --max-procs=$MAX_PROCS bash -c "CMD"
+done | xargs -I CMD --max-procs=$MAX_PROCS timeout $TIMEOUT bash -c "CMD"
