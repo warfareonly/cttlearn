1d8c7db0c5925f053c506c861a07c40576f04b1a
Diego Damasceno <d.damasceno@cs.ru.nl>
2021-11-23 16:49:52 +0100

    using w and wp from LearnLib

--- experiments/02ctt_learning.sh
+++ experiments/02ctt_learning.sh
@@ -6,9 +6,9 @@ MAX_PROCS=7
 TIMEOUT=30m
 
 # for EXTRA_STATES in 0 1 2; do
-#     for CTT in soucha_w soucha_wp soucha_h soucha_hsi soucha_spy soucha_spyh; do
-for EXTRA_STATES in 0 1; do
-    for CTT in soucha_w soucha_wp soucha_hsi soucha_spy; do
+#     for CTT in w wp soucha_h soucha_hsi soucha_spy soucha_spyh; do
+for EXTRA_STATES in 0 1 2; do
+    for CTT in w wp soucha_hsi soucha_spy; do
         for SUL in  Bankcard DTLS MQTT QUICprotocol SSH TCP TLS; do
             for F_DOT in "${MODEL_DIR}/${SUL}/"*.dot; do 
                 echo $CTT_LEARNING -eq $CTT -es $EXTRA_STATES -sul "$F_DOT"
