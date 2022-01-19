10230b9eaf88ca1ac010a4fddf91d65213a1ef20
Diego Damasceno <d.damasceno@cs.ru.nl>
2022-01-19 15:36:23 +0100

    EQWrapperHypSize logging fixed

--- src/cttlearning/src/main/java/nl/ru/icis/oracle/EQWrapperHypSize.java
+++ src/cttlearning/src/main/java/nl/ru/icis/oracle/EQWrapperHypSize.java
@@ -14,31 +14,26 @@ import net.automatalib.words.Word;
 public class EQWrapperHypSize<I, O> implements MealyEquivalenceOracle<I, O> {
 	
 	private final LearnLogger LOGGER = LearnLogger.getLogger(EQWrapperHypSize.class);
-	private MealyEquivalenceOracle<I, O> eq_oracle;
-	private int iteration_num = 1; 
+	private MealyEquivalenceOracle<I, O> eqOracle;
+	private int iterationNum = 1; 
 	
 	public EQWrapperHypSize(MealyEquivalenceOracle<I, O> oracle) {
-		this.eq_oracle = oracle;
+		this.eqOracle = oracle;
 	}
 	
 	public EQWrapperHypSize(EquivalenceOracle<MealyMachine<?, I, ?, O>, I, Word<O>> oracle) {
-		this.eq_oracle = (MealyEquivalenceOracle<I,O>) oracle;
+		this.eqOracle = (MealyEquivalenceOracle<I,O>) oracle;
 	}
 
 	@Override
 	public @Nullable DefaultQuery<I, Word<O>> findCounterExample(MealyMachine<?, I, ?, O> hypothesis, Collection<? extends I> inputs) {
-		String log_formatted = null;
+		LOGGER.logEvent(String.format("EqOIteration=%d", iterationNum));
+		LOGGER.logEvent(String.format("EqOHypSize=%d", hypothesis.size()));
+		iterationNum++;
 		
-		log_formatted = String.format("EQWrapperHypSize: {Iteration=%d;HypothesisSize=%d;}", iteration_num,hypothesis.size());
-		LOGGER.logEvent(log_formatted);
-		iteration_num++;
-		
-		String eq_str = "";
-		DefaultQuery<I, Word<O>> eq_return = this.eq_oracle.findCounterExample(hypothesis, inputs);
-		if (eq_return != null) eq_str = eq_return.getInput().toString();
-		log_formatted = String.format("EQWrapperHypSize: {CE=%s;}", eq_str);
-		LOGGER.logEvent(log_formatted);
-		return eq_return;
+		DefaultQuery<I, Word<O>> qryToReturn = this.eqOracle.findCounterExample(hypothesis, inputs);
+		LOGGER.logEvent(String.format("EqOCE=%s", String.valueOf(qryToReturn)));
+		return qryToReturn;
 	}
 
 }
