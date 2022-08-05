a85f5b8d7e9affa459b6640e8f466372df2ec8f5
Diego Damasceno <d.damasceno@cs.ru.nl>
2022-08-05 17:24:05 +0200

    Adding statsSUL to log

--- src/cttlearning/src/main/java/nl/ru/icis/oracle/EQWrapperHypSize.java
+++ src/cttlearning/src/main/java/nl/ru/icis/oracle/EQWrapperHypSize.java
@@ -1,6 +1,8 @@
 package nl.ru.icis.oracle;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -8,6 +10,7 @@ import de.learnlib.api.logging.LearnLogger;
 import de.learnlib.api.oracle.EquivalenceOracle;
 import de.learnlib.api.oracle.EquivalenceOracle.MealyEquivalenceOracle;
 import de.learnlib.api.query.DefaultQuery;
+import de.learnlib.api.statistic.StatisticSUL;
 import net.automatalib.automata.transducers.MealyMachine;
 import net.automatalib.words.Word;
 
@@ -16,6 +19,7 @@ public class EQWrapperHypSize<I, O> implements MealyEquivalenceOracle<I, O> {
 	private final LearnLogger LOGGER = LearnLogger.getLogger(EQWrapperHypSize.class);
 	private MealyEquivalenceOracle<I, O> eqOracle;
 	private int iterationNum = 0; 
+	private Map<String,StatisticSUL> statisticSuls = new HashMap<>();
 	
 	public EQWrapperHypSize(MealyEquivalenceOracle<I, O> oracle) {
 		this.eqOracle = oracle;
@@ -23,6 +27,10 @@ public class EQWrapperHypSize<I, O> implements MealyEquivalenceOracle<I, O> {
 	
 	public EQWrapperHypSize(EquivalenceOracle<MealyMachine<?, I, ?, O>, I, Word<O>> oracle) {
 		this.eqOracle = (MealyEquivalenceOracle<I,O>) oracle;
+	}
+	
+	public void addStatisticsSUL(String name, StatisticSUL stats) {
+		this.statisticSuls.put(name, stats);
 	}
 
 	@Override
@@ -35,6 +43,12 @@ public class EQWrapperHypSize<I, O> implements MealyEquivalenceOracle<I, O> {
 		}
 		iterationNum++;
 		LOGGER.logEvent(String.format("EQStats:{'Iter':%d,'HypSize':%d,'CESize':%d}", iterationNum, hypothesis.size(), qry_size));
+		
+		for (String k : this.statisticSuls.keySet()) {
+			LOGGER.logEvent(String.format("EQStats:{%s}", this.statisticSuls.get(k).getStatisticalData().toString()));
+			
+		}
+		
 		return qryToReturn;
 	}
 
